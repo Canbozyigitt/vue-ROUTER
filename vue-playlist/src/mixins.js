ae32484d9f7a13c750f66b3d5ca81251e3c744e4
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-08-08 00:03:00 +0300

    Update mixins.js

--- vue-playlist/src/mixins.js
+++ vue-playlist/src/mixins.js
@@ -1,6 +1,6 @@
 export default {
     computed: {
-        // 'filteredBlogs' adlı computed property, 'blogs' dizisindeki blog yazılarını filtreler
+        // 'filteredBlogs' adlı computed property, 'blogs' dizisindeki blog yazılarını filtreler.
         filteredBlogs() {
             // 'blogs' dizisini filtreler ve 'title' özelliğinde 'keyword' ile eşleşen blog yazılarını döndürür
             return this.blogs.filter(blog => blog.title.match(this.keyword));
